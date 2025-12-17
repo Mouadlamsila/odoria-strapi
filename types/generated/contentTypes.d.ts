b54043fdba8b880c771ca8659ad04566e47bea80
Mouadlamsila <mouad.lamssila@gmail.com>
2025-12-17 10:21:47 +0100

    add field phone in user

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1428,6 +1428,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    phone: Schema.Attribute.String;
     provider: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     resetPasswordToken: Schema.Attribute.String & Schema.Attribute.Private;
